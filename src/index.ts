ca887995a908685d0348d34965fa50da2acea3b8
SkyNaoto <naoto_70102@yahoo.co.jp>
2024-07-13 12:42:23 +0900

    section 24

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Observable, Subscriber } from 'rxjs';
 const emptyObservables = new Observable<string>( subscriber => {
   console.log('Observable executed');
   subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(()=> subscriber.next('Charlie'),2000);
 });
 
 console.log('Before subscribe');
