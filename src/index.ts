b18730bc6a32bafb7b78f1d841b01eff7ba49b2b
SkyNaoto <naoto_70102@yahoo.co.jp>
2024-07-13 12:17:57 +0900

    section 22

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 
-const someObservable$ = new Observable<string>(subscriber => {
+const emptyObservables = new Observable<string>( subscriber => {
   console.log('Observable executed');
-  subscriber.next('Alice');
-  setTimeout(()=> subscriber.next('Ben'),2000);
-  setTimeout(()=> subscriber.next('Charlie'),4000);
-  // subscriber.complete();
 });
 
-console.log('Subscription 1 starts');
-someObservable$.subscribe(value => console.log('Subscription 1:',value));
+console.log('Before subscribe');
+emptyObservables.subscribe(value=>console.log('Subscription:',value));
+console.log('After subscribe');
 
-setTimeout(()=>{
-  console.log('Subscription 2 starts');
-  someObservable$.subscribe(value => console.log('Subscription 2:',value));
-},1000);
+// const someObservable$ = new Observable<string>(subscriber => {
+//   console.log('Observable executed');
+//   subscriber.next('Alice');
+//   setTimeout(()=> subscriber.next('Ben'),2000);
+//   setTimeout(()=> subscriber.next('Charlie'),4000);
+//   // subscriber.complete();
+// });
+
+// console.log('Subscription 1 starts');
+// someObservable$.subscribe(value => console.log('Subscription 1:',value));
+
+// setTimeout(()=>{
+//   console.log('Subscription 2 starts');
+//   someObservable$.subscribe(value => console.log('Subscription 2:',value));
+// },1000);
